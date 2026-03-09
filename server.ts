36960c2176df1b9b6714183c0b91c2e965be704c
Jason Luo-Xia <z5693060@ad.unsw.edu.au>
2026-03-09 20:27:13 +1100

    changed to ts

--- server.ts
+++ server.ts
@@ -3,11 +3,13 @@ import { express } from 'express';
 import {open } from 'open';
 import { SpotifyWebApi } from 'spotify-web-api-node';
 
+const app = express();
+const PORT = process.env.PORT || 8888;
+
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     redirectUrl: process.env.SPOTIFY_REDIRECT_URL
 });
 
-const app = express();
-const PORT = process.env.PORT || 8888;
+const SCOPES = ['user-top-read', 'user-read-private', 'user-read-email'];
